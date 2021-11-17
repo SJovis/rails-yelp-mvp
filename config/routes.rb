8f44171759692fc4ab087079dbbd3bcff2408ee8
Sergio Cerqueira <jovis.sergio@gmail.com>
2021-11-17 16:54:39 +0000

    challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get :restaurants, to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
   post :restaurants, to: 'restaurants#create'
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
