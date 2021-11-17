96d82599f504591778c1c994a11e69b184ac4da1
Sergio Cerqueira <jovis.sergio@gmail.com>
2021-11-17 16:34:45 +0000

    restaurant controller completed

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0 }
-  validates :rating, numericality: { less_than: 6 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create({
+        name: "La Tour d'Argent",
+        address: "15 Quai de la Tournelle, 75005 Paris",
+        phone_number: "01 43 54 23 31",
+        category: "french"})
+Restaurant.create({
+        name: "K.O.B",
+        address: "15 Quai de la Tournelle, 75005 Paris",
+        phone_number: "934 000 949",
+        category: "italian"})
+Restaurant.create({
+        name: "O Talho",
+        address: "R. Carlos Testa 1B, 1050-046 Lisboa",
+        phone_number: "21 315 4105",
+        category: "belgian"})
+Restaurant.create({
+        name: "Guilty by Olivier, Avenida",
+        address: "R. Barata Salgueiro 28 A, 1250-044 Lisboa",
+        phone_number: "21 191 3590",
+        category: "chinese"})
+Restaurant.create({
+        name: "Yakuza by Olivier, Lisboa",
+        address: "Hotel Avani, R. Júlio César Machado 7, 1250-135 Lisboa",
+        phone_number: "934 000 913",
+        category: "japanese"})
